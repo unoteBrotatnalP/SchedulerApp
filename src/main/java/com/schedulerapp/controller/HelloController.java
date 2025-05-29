23ff1af340fbe2135367d05e0233eb6c04a0cc09
Michal Wendel <michal.wendel.up@gmail.com>
2025-05-29 17:55:37 +0200

    Testowe Hello

---
+++ src/main/java/com/schedulerapp/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.schedulerapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello";
+    }
+}
